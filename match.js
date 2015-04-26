68e205e58c8da553cdcd6832d209e4f987b80cbc
Bew Harichanwong <bew.hari@gmail.com>
2015-04-25 21:55:19 -0500

    Add waiting queue

--- match.js
+++ match.js
@@ -1,9 +1,11 @@
 module.exports = function(io, waitingQueue){
-	io.on('connection', function(socket) {
+	io.on('connection', function(socket){
 		console.log('Player ' + socket.id + ' connected.');
 		
-		socket.on('join queue', function(){
-			
-		});
+		socket.on('join queue', function(msg){
+	    console.log(socket.id + ' joined queue');
+	    waitingQueue.push(socket.id);
+	    console.log(waitingQueue[0]);
+	  });
 	});
 }
--- public/js/chat.js
+++ public/js/chat.js
@@ -1,9 +1,17 @@
 $(function(){
 	var socket = io();
 
-	$('#join').on('click', function(){
-		//socket.emit('join queue');
-		alert('button!');
+	$('#join').on('click', function(e){
+		//e.preventDefault();
+
+		socket.emit('join queue', null);
+		//alert('button!');
 	});
+
+	$('#textbox').submit(function(){
+    //socket.emit('join queue', null);
+    $('#m').val('');
+    return false;
+  });
 
 });
